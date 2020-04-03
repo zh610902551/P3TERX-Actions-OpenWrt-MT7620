71077064104f87dd04bc6837fae72f59879b614d
zh610902551 <610902551@qq.com>
2020-04-03 13:54:08 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,11 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+- name: Update luci-app-ssr-plus-Jo
+        run: |
+git clone -b lean  https://github.com/Sweet-Fairy/luci-app-ssr-plus-Jo package/luci-app-ssr-plus-lean
+
+- name: Update feeds
+        run: |
+          ./scripts/feeds update -a
+          ./scripts/feeds install -a
