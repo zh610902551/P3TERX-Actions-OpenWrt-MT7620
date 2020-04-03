0054b7c469813c8d0df7226cc2371c6f35110d68
zh610902551 <610902551@qq.com>
2020-04-03 21:41:25 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,7 +9,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 - name: Update luci-app-ssr-plus-Jo
         run: |
-git clone -b lean  https://github.com/Sweet-Fairy/luci-app-ssr-plus-Jo package/luci-app-ssr-plus-lean
+git clone https://github.com/Sweet-Fairy/luci-app-ssr-plus-Jo package/luci-app-ssr-plus-lean
 
 - name: Update feeds
         run: |
